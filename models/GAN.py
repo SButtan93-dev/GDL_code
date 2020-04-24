bf7a83171a312aa9c6fb6320b8a9d1c2bb5de42e
Carlos Castellanos <carloscastellanossf@gmail.com>
2020-04-24 18:10:47 -0400

    fixed bug in one-sided label smoothing of discriminator

--- models/GAN.py
+++ models/GAN.py
@@ -343,8 +343,8 @@ class GAN():
         # from Goodfellow, I. (2016). NIPS 2016 Tutorial: Generative Adversarial Networks.
         # https://arxiv.org/abs/1701.00160
         # I'm doing this for the fake examples too.
-        valid = np.random.uniform(low=0.9, high=1.0, size=batch_size)
-        fake = np.random.uniform(low=0.0, high=0.1, size=batch_size)
+        valid = np.random.uniform(low=0.9, high=1.0, size=(batch_size,1))
+        fake = np.random.uniform(low=0.0, high=0.1, size=(batch_size,1))
         #valid = np.ones((batch_size,1))
         #fake = np.zeros((batch_size,1))
 
